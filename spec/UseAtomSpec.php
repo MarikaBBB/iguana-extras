f4fd691f091156d580adfc14faddf3de4010974c
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-01-14 11:37:45 +0000

    convert init() function tests from phpUnit to Kahlan

--- spec/UseAtomSpec.php
+++ spec/UseAtomSpec.php
@@ -20,11 +20,45 @@ describe('UseAtom', function() {
             \WP_Mock::expectActionAdded('init', [$this->useAtom, 'init']);
             \WP_Mock::expectActionAdded('wp_head', [$this->useAtom, 'wpHead']);
     
-            $this->useAtom->register();
-            
+            $this->useAtom->register();           
+        });
+    });
+
+    describe('::init()', function() {
+        
+        it('starts correclty by removing filters and actions', function() {
+            \WP_Mock::expectFilterAdded('default_feed', [$this->useAtom, 'defaultFeed']);
+
+            $this->useAtmon->init();
         });
 
+        it("removes the actions", function() {
+            
+            \WP_Mock::wpFunction('remove_action', [
+                'args' => ['do_feed_rdf', 'do_feed_rdf', 10, 1],
+                'times' => 1,
+            ]);
+            \WP_Mock::wpFunction('remove_action', [
+                'args' => ['do_feed_rss', 'do_feed_rss', 10, 1],
+                'times' => 1,
+            ]);
+            \WP_Mock::wpFunction('remove_action', [
+                'args' => ['do_feed_rss2', 'do_feed_rss2', 10, 1],
+                'times' => 1,
+            ]);
+
+            $this->useAtom->init();
+        });
+
+        // Check that this code runs to completion.
+        it('completes execution without errors', function() {
+            expect(function() {
+                $this->useAtom->init();
+            })->not->toThrow();
+        });
     });
+
+    
 
 
     
